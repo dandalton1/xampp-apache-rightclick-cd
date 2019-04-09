ae4fbd84789c148655ffef3de4b06caa8f9eaafc
Danny Dalton <daltond2@hawkmail.newpaltz.edu>
2019-04-09 12:11:11 -0400

    Initial commit

---
+++ xampp-apache-rightclick-cd/Parser.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace xampp_apache_rightclick_cd
+{
+    class Parser
+    {
+        private string filename;
+        private string workingDir;
+        private bool debug;
+        
+        public Parser(string fName, string wDir, bool d)
+        {
+            filename = fName;
+            workingDir = wDir;
+            debug = d;
+        }
+
+        public void Parse()
+        {
+            if (File.Exists(filename))
+            {
+                StreamReader reader = new StreamReader(filename);
+                string line = "";
+                StringBuilder stringBuilder = new StringBuilder();
+                TextWriter writer = debug ? Console.Out : new StringWriter(stringBuilder);
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.StartsWith("DocumentRoot"))
+                    {
+                        writer.WriteLine("DocumentRoot \"" + workingDir + "\"");
+                        line = reader.ReadLine(); // trash the next line of the file, since it's more than likely a "Directory" directive
+                        writer.WriteLine("<Directory \"" + workingDir + "\">");
+                        writer.WriteLine(reader.ReadToEnd()); // write all of the rest of the file
+                        break; // end parse
+                    }
+                    else
+                    {
+                        writer.WriteLine(line);
+                    }
+                }
+
+                reader.Close();
+
+                if (!debug)
+                {
+                    StreamWriter fileWriter = new StreamWriter(filename);
+
+                    fileWriter.WriteLine(stringBuilder.ToString());
+
+                    fileWriter.Flush();
+
+                    fileWriter.Close();
+                }
+            } else
+            {
+                throw new FileNotFoundException("XAMPP Apache httpd.conf file " + filename + " not found.");
+            }
+        }
+    }
+}
---
+++ xampp-apache-rightclick-cd/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using System;
+using System.Threading;
+
+namespace xampp_apache_rightclick_cd
+{
+    class Program
+    {
+        /**
+         * Change me if your http_conf is NOT C:\xampp\apache\conf\httpd.conf
+         * 
+         * C# also needs to replace one backslash with two (C:\xampp -> C:\\xampp)
+         **/
+        static string httpd_conf = "C:\\xampp\\apache\\conf\\httpd.conf";
+
+        /**
+         * Use for debugging
+         **/
+        static bool Debug = false;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                string wd = Directory.GetCurrentDirectory();
+
+                Console.WriteLine("Working directory: " + wd);
+
+                Parser p = new Parser(httpd_conf, wd, Debug);
+
+                p.Parse();
+
+                pause();
+            } catch (Exception e)
+            {
+                err(e);
+            }
+        }
+
+        static void err(Exception e)
+        {
+            Console.Error.WriteLine("ERROR: " + e.Message);
+            Console.Error.WriteLine(e.StackTrace);
+            pause();
+            
+        }
+
+        static void pause()
+        {
+            if (Debug)
+            {
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
